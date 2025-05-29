ec8aafbb43309e5507da54b566c2ccdbc149cc2c
Ruhan105 <149739823+Ruhan105@users.noreply.github.com>
2025-05-29 01:50:21 +0100

    Completed 9.2.2

--- exercises.py
+++ exercises.py
@@ -1,43 +1,50 @@
-# Excersise 9.2.1
+# Excersise 9.2.1/2
 a = 1.5
 b = 1.0
 X0 = 1.0
+fine_resolution = 2**10
+t0 = 0
+tn = 1
 
 
-def actualSolution():
+def actualSolution(a, b, X0, n):
     """Generates a sample path using the exact solution of the SDE in 9.2.1"""
-    n = 2**9
+
+    n = 2**10
 
     dt = 1/n
 
-    increments = [np.random.normal(loc=0, scale=np.sqrt(dt)) for _ in range(n)]
-    total_increments = [increments[0]]
-    for i in increments[1:]:
-        total_increments.append(total_increments[-1] + i)
+    dW_fine = [np.random.normal(loc=0, scale=np.sqrt(dt)) for _ in range(n)]
+    W_fine = np.cumsum(dW_fine)
+    sample_points = []
 
-    points = []
+    for t, w in enumerate(W_fine):
+        sample_points.append(X0*np.exp((a - (b/2)**2)*(t/n) + b*w))
 
-    for t, w in enumerate(total_increments):
-        points.append(1*np.exp((a - (b/2)**2)*(t/n) + b*w))
-    
-    return points
+    return sample_points, W_fine
 
+# Compute Euler Approximation
 
-dt = 2**-2
-N = 4
+dt = 2**-4
+N = int(1/dt)
+
+points, W_fine = actualSolution(a, b, X0, fine_resolution)
+
+# Using the same sample paths, skipping enough terms for fine case
+W_coarse = W_fine[::int((dt*fine_resolution))]
+dW_coarse = np.diff(W_coarse)
 
 X_approx = [1.0]
 
-# Euler approximation of SDE
-for _ in range(N-1):
-    dW = np.random.normal(loc=0, scale=dt)
+for dW in dW_coarse:
     Yn = X_approx[-1]
     X_approx.append(Yn + a*Yn*dt + b*Yn*dW)
 
 
 fig, ax = plt.subplots()
-ax.plot(np.linspace(start=0, stop=1, num=2**9), actualSolution())
+ax.plot(np.linspace(start=t0, stop=tn, num=fine_resolution), points)
 
-plt.plot(np.linspace(0,1,N), X_approx)
+plt.plot(np.linspace(t0, tn, N), X_approx)
 
 plt.show()
+
